f3af01d77f031b5b522cf7b721c6f4f77e4c06a7
brandonlambrecht <brandonjlambrecht@gmail.com>
2024-01-20 16:52:16 -0600

    added projects,added form into contact page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,3 +1,44 @@
 export default function ContactPage() {
-    return "Contact"
+    return (
+    <div className="container">
+  <div className="row header">
+    <h1>Contact Me</h1>
+  </div>
+  <div className="row body">
+    <form action="#">
+      <ul>
+        
+        <li>
+          <p className="left">
+            <label for="first_name">first name</label>
+            <input type="text" name="first_name" placeholder="First name" />
+          </p>
+          <p className="pull-right">
+            <label for="last_name">last name</label>
+            <input type="text" name="last_name" placeholder="Last name" />      
+          </p>
+        </li>
+        
+        <li>
+          <p>
+            <label for="email">email <span className="req">*</span></label>
+            <input type="email" name="email" placeholder="Email" />
+          </p>
+        </li>        
+        <li><div className="divider"></div></li>
+        <li>
+          <label for="comments">comments</label>
+          <textarea cols="46" rows="3" name="comments"></textarea>
+        </li>
+        
+        <li>
+          <input className="btn btn-submit" type="submit" value="Submit" />
+          <small>or press <strong>enter</strong></small>
+        </li>
+        
+      </ul>
+    </form>  
+  </div>
+</div>
+    )
 }
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,39 +1,42 @@
+import {Link} from "react-router-dom"
+
 export default function ProjectPage() {
+    return (
+    
+    <section className="container-content">
 
-    return "Projects"
-// (
-//     <section className="container-content">
+    <div className="title-wrap">
+        <h2 id="work">Projects</h2>
+    </div>
+    <div className="content-wrap">
+        <div id="card1">
+            <Link to="https://github.com/brandonlambrecht/weather-dashboard">PawFit</Link>
+        </div>
+        <div id="card2">
+                    <div>
+                        <Link to="https://github.com/brandonlambrecht/work-day-scheduler>Workday Scheduler">The Local List</Link>
+                    </div>
+                </div>
+        <div id="card3">
+                    <div>
+                        <Link to="https://github.com/brandonlambrecht/work-day-scheduler>Workday Scheduler">README Generator</Link>
+                    </div>
+                </div>
 
-//     <div className="title-wrap">
-//         <h2 id="work">Work</h2>
-//     </div>
+        <div id="card4">
+                    <div>
+                        <Link to="https://github.com/brandonlambrecht/work-day-scheduler>Workday Scheduler">Social Network</Link>
+                    </div>
+                </div>
 
-//     <div className="content-wrap">
-//         <div id="card1">
-//             <a href=https://github.com/brandonlambrecht/weather-dashboard>Weather Dashboard</a>
-//         </div>
+        <div id="card5">
+                    <div>
+                        <Link to="https://github.com/brandonlambrecht/work-day-scheduler>Workday Scheduler">Text Editor</Link>
+                    </div>
+                </div>
 
-//         <div id="card2">
-//             <div>
-//                 <a href=https://github.com/brandonlambrecht/work-day-scheduler>Workday Scheduler</a>
-//             </div>
-//         </div>
-//         <div id="card3">
-//             <div>
-//                 <a href="https://github.com/brandonlambrecht/password-generator">Password Generator</a>
-//             </div>
-//         </div>
-//         <div id="card4">
-//             <div>
-//                 <a href="https://github.com/brandonlambrecht?tab=repositories">Project 4</a>
-//             </div>
-//         </div>
-//         <div id="card5">
-//             <div>
-//                 <a href="https://github.com/brandonlambrecht?tab=repositories">Project 5</a>
-//             </div>
-//         </div>
-//     </div>
-//     </section>
-// )
+        
+    </div>
+    </section>
+)
 }
